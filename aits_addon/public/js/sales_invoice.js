c90bd47345eb1cb39caccadf9ad0f9cc013f9467
uNik020 <shekharnikhil020@gmail.com>
2026-05-18 06:39:22 +0000

    credit note new condtion3

--- aits_addon/public/js/sales_invoice.js
+++ aits_addon/public/js/sales_invoice.js
@@ -1,7 +1,7 @@
 frappe.ui.form.on("Sales Invoice", {
 	is_return: function(frm) {
 		if (frm.doc.is_return) {
-			frm.set_value("update_stock", 1);
+			frm.set_value("update_stock", 0);
 		}
 	}
 });
